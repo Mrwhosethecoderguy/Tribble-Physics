244d2e756e6a0ba6e9aa99bbc73f9c61dfc0bd08
Mrwhoisthecoderguy <harsh.gumma@gmail.com>
2023-05-29 17:55:28 +0530

    Added vector class

---
+++ vector.py
@@ -0,0 +1,25 @@
+import math
+import numpy as np
+
+class vector:
+	def __init__(self,_x=0,_y=0):
+		self.x = _x
+		self.y = _y
+		self.mag = math.sqrt(self.x**2+self.y**2)
+
+		if self.y > 0 and self.x > 0:
+			self.ang = math.atan(self.y/self.x)
+		elif self.y > 0 and self.x < 0:
+			self.ang = math.pi/2+math.atan(self.y/self.x)
+		elif self.y < 0 and self.x < 0:
+			self.ang = math.pi+math.atan(self.y/self.x)
+		elif self.y < 0 and self.x > 0:
+			self.ang = 3*math.pi/2 + math.atan(self.y/self.x)
+
+	def __add__(self,vec):
+		return vector(self.x+vec.x,self.y+vec.y)
+
+	def __sub__(self,vec):
+		return vector(self.x-vec.x,self.y-vec.y)
+	def __mul__(self,vec):
+		return vector(self.x*vec,self.y*vec)
